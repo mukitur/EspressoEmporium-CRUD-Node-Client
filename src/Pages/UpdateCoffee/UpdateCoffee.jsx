2d0a7559a5128dc25050116f543c87291526a4fc
mukitur <mukit.inway@gmail.com>
2023-10-14 18:02:04 +0600

    project setup

---
+++ src/Pages/UpdateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,9 @@
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h2>update coffee</h2>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainRoot from '../layout/MainRoot';
+import Home from '../Pages/Home/Home';
+import AddCoffee from '../Pages/AddCoffee/AddCoffee';
+import UpdateCoffee from '../Pages/UpdateCoffee/UpdateCoffee';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainRoot />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/addcoffee',
+        element: <AddCoffee />,
+      },
+      {
+        path: '/updatecoffee',
+        element: <UpdateCoffee />,
+      },
+    ],
+  },
+]);
+
+export default router;
